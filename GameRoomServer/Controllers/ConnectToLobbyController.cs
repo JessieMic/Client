bd3fef9eccbed4759f24ef8147243e84277688dd
Idan Oren <idanor@mta.ac.il>
2023-04-13 15:12:50 +0300

    added sever project

---
+++ GameRoomServer/Controllers/ConnectToLobbyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameRoomServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConnectToLobbyController: ControllerBase
+    {
+        [HttpGet(Name = "connectToLobby")]
+        public IActionResult Get()
+        {
+            string ip = HttpContext.Connection.RemoteIpAddress.ToString();
+            if (HttpContext.Request.Headers.ContainsKey("X-Forwarded-For"))
+            {
+                ip = HttpContext.Request.Headers["X-Forwarded-For"];
+            }
+            return Ok(ip);
+        }
+    }
+}
