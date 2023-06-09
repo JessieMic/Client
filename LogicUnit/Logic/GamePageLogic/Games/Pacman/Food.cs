7752dde1dc09a70de6a502bf939ad6cdb5241805
noajhirad <noaj1997@gmail.com>
2023-06-09 14:52:18 +0300

    placed pacman objects on screen and tried to work on movement

--- LogicUnit/Logic/GamePageLogic/Games/Pacman/Food.cs
+++ LogicUnit/Logic/GamePageLogic/Games/Pacman/Food.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿using Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Objects;
 
 namespace LogicUnit.Logic.GamePageLogic.Games.Pacman
 {
-    public class Ghost : GameObject
+    public class Food : GameObject
     {
-
     }
 }
---
+++ LogicUnit/Logic/GamePageLogic/Games/Pacman/GhostObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Objects;
+
+namespace LogicUnit.Logic.GamePageLogic.Games.Pacman
+{
+    public class GhostObject : GameObject
+    {
+        private int[,] m_Board;
+
+        public GhostObject(ref int[,] i_Board)
+        {
+            m_Board = i_Board;
+        }
+    }
+}
